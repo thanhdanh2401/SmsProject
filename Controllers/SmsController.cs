d4e82bf64565e4af1a1921830175f862768aa17f
thanhdanh2401 <lephamthanhdanh@gmail.com>
2021-10-01 15:30:11 +0700

    first commit

---
+++ Controllers/SmsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SmsProject.Models;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+
+namespace SmsProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SmsController : TwilioController
+    {
+        private readonly ITwilioRestClient _client;
+        public SmsController(ITwilioRestClient client)
+        {
+            _client = client;
+        }
+        //Send sms to client
+        [HttpGet]
+        public IActionResult SendSms(SmsMessage model)
+        {
+            var message = MessageResource.Create(
+                to: new PhoneNumber(model.To),
+                from: new PhoneNumber(model.From),
+                body: model.Message,
+                client: _client); 
+            return Content(message.Sid);//Show security log to know if success
+        }
+    }
+}
